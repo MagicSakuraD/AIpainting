f3f619eb2419348a9d7317e11a52ff6bfda42238
MagicSakuraD <55037538+MagicSakuraD@users.noreply.github.com>
2023-03-31 11:07:00 +0800

    delete commit

--- src/openai.js
+++ src/openai.js
@@ -1,21 +1,5 @@
 import { Configuration, OpenAIApi } from "openai";
-// const configuration = new Configuration({
-//   organization: "org-L54T8evqRXzJVYIdfeXIReqd",
-//   apiKey: "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",
-// });
-// const openai = new OpenAIApi(configuration);
-// const response = await openai.listEngines();
-// const { Configuration, OpenAIApi } = require("openai");
-// const configuration = new Configuration({
-//   apiKey: "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",
-// });
-// const openai = new OpenAIApi(configuration);
-// const response = await openai.createCompletion({
-//   model: "text-davinci-003",
-//   prompt: "Say this is a test",
-//   temperature: 0,
-//   max_tokens: 7,
-// });
+
 
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
